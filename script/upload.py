bfcc3039748fb06cfda1805f1f9f0ddcb09468af
Aber <me@abersheeran.com>
2022-07-15 00:44:41 +0800

    Initial commit

---
+++ script/upload.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+from pathlib import Path
+
+here = Path(__file__).absolute().parent.parent
+
+package_name = "coolsql"
+
+
+def get_version(package: str = package_name) -> str:
+    """
+    Return version.
+    """
+    _globals: dict = {}
+    exec((here / package / "__version__.py").read_text(encoding="utf8"), _globals)
+    return _globals["__version__"]
+
+
+os.chdir(here)
+subprocess.check_call(f"poetry version {get_version()}", shell=True)
+subprocess.check_call(
+    f"git add {package_name}/__version__.py pyproject.toml", shell=True
+)
+subprocess.check_call(f'git commit -m "{get_version()}"', shell=True)
+subprocess.check_call("git push", shell=True)
+subprocess.check_call("git tag v{0}".format(get_version()), shell=True)
+subprocess.check_call("git push --tags", shell=True)
